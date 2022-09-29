242411a7be73b185503ba5dbb2824c41ab807627
anonimussapee <librededrogas420@gmail.com>
2022-09-28 21:11:07 -0500

    escribi el resultado

--- mokepón/js/mokepon.js
+++ mokepón/js/mokepon.js
@@ -1,18 +1,19 @@
 let ataqueJugador
+let ataqueEnemigo
 
 function iniciarJuego(){
         let botonMascotaJugador = document.getElementById("boton-mascota");
         botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador);
-        let botonAtaqueJugador
+        
 
         let botonFuego = document.getElementById("boton-fuego")
-        botonFuego.addEventListener('click', botonFuego);
+        botonFuego.addEventListener('click', ataqueFuego);
 
         let botonAgua = document.getElementById("boton-agua")
-        botonAgua.addEventListener('click',botonAgua)
+        botonAgua.addEventListener('click',ataqueAgua)
 
         let botonTierra = document.getElementById("boton-tierra")
-        botonTierra.addEventListener('click', botonTierra)
+        botonTierra.addEventListener('click', ataqueTierra)
      }
     function seleccionarMascotaJugador(){
 
@@ -35,31 +36,77 @@ function iniciarJuego(){
         }
         
       function seleccionarMascotaEnemigo(){
-        let ataqueAleatorio = aleatorio (1, 3)
+        let mascotaAleatoria = aleatorio (1, 3)
         let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
 
-        if (ataqueAleatorio == 1) {
+        if (mascotaAleatoria == 1) {
           spanMascotaEnemigo.innerHTML =  "Hipodoge"
-            }else if(ataqueAleatorio == 2) {
+            }else if(mascotaAleatoria == 2) {
 
           spanMascotaEnemigo.innerHTML =  "Capipepo"
             }else {
           spanMascotaEnemigo.innerHTML =  "Ratigueya"
              
             }
-        function ataqueFuego(){
-          ataqueJugador = "FUEGO"
-          alert(ataqueJugador)
-        } function ataqueAgua(){
-          ataqueJugador = "AGUA"
-          alert(ataqueJugador)
-        }function ataqueTierra(){
-            ataqueJugador = "TIERRA"
-            alert(ataqueJugador)
+        
           }
+          function ataqueFuego(){
+            ataqueJugador = "FUEGO"
+            ataqueAleatorioEnemigo()
 
-            
-      }
+          } function ataqueAgua(){
+            ataqueJugador = "AGUA"
+            ataqueAleatorioEnemigo()
+
+
+          }function ataqueTierra(){
+              ataqueJugador = "TIERRA"
+              ataqueAleatorioEnemigo()
+            }
+      
+              function ataqueAleatorioEnemigo () {
+                let ataqueAleatorio = aleatorio(1, 3)
+                
+                
+                if(ataqueAleatorio == 1){
+                  ataqueEnemigo = 'FUEGO'
+                } else if (ataqueAleatorio == 2){
+                    ataqueEnemigo = 'AGUA'
+                } else {
+                  ataqueEnemigo = 'TIERRA'
+                }
+
+                combate()
+              }
+
+              function combate(){
+                if (ataqueEnemigo == ataqueJugador ) {
+                  crearMensaje('EMPATE')
+
+                } else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA' ){
+                  crearMensaje('GANASTE')
+
+                } else if (ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO' ){
+                  crearMensaje('GANASTE')
+
+                } else if ( ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA' ){
+                  crearMensaje('GANASTE')
+
+              } else{
+                crearMensaje('PERDISTE')
+              }
+              }
+
+              function crearMensaje(resultado) {
+                let sectionMensajes = document.getElementById('mensajes')
+
+                let parrafo = document.createElement('p')
+                
+                parrafo.innerHTML = 'tu mascota ataco con '  + ataqueJugador + ', la mascota del ataco con ' +  ataqueEnemigo + '- ' + resultado 
+                
+                sectionMensajes.appendChild(parrafo)
+              }
+
         function aleatorio(min, max) {
           return Math.floor((Math.random() * (max - min + 1)) + min);
       }
