afd266fe83833b69304ae22b76a01747e550f134
anonimussapee <librededrogas420@gmail.com>
2023-01-30 15:07:49 -0500

    agrege los efectos a los personajes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,18 +1,18 @@
 var countdown="https://ypok.netlify.app/src/img/fondos/countdown.webp";
 
-var pokedec=[{"name":"https://ypok.netlify.app/src/img/fondos/charizard.gif","power":1000,"atack":[{"name":"Garra\n Metal","power":50},{"name":"Puño\n Fuego","power":60},{"name":"Colmillo\n Ígneo","power":65},{"name":"Cuchillada","power":50}], "title":"Charizard"}/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/fuecoco.gif","power":800, "title":"Fuecoco","atack":[{"name":"Mordisco","power":50},{"name":"Calcinación","power":55},{"name":"Lanzallamas","power":65},{"name":"Alarido","power":50}]}/*sig
-*/,{"name":"https://ypok.netlify.app/src/img/fondos/rowlet.gif","power":700, "title":"Rowlet","atack":[{"name":"Picotazo","power":50},{"name":"Hoja\n afilada","power":60},{"name":"Ataque\n Furía","power":65},{"name":"Hoja\n Aguda","power":70}]}/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/bulbasaur.gif","power":500, "title":"Bulbazur","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"veneno"},{"tipo":"planta"}]}/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/squirtle.gif","title":"Squirtle","power":"500","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"agua"}]}/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/pikachu.gif","power":500, "title":"pikachu","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"electrico"}],"evolucion":[{"name":"raychu","img":{"img":"https://ypok.netlify.app/src/img/fondos/raichu.gif"},"power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]}/*sig
+var pokedec=[{"img":"https://ypok.netlify.app/src/img/fondos/charizard.gif","power":1000,"atack":[{"name":"Ataque\n Ala","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"},{"name":"Pirotecnia","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"},{"name":"Colmillo\n Ígneo","power":70,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"},{"name":"Cuchillada","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"}], "title":"Charizard","tipo":[{"tipo":"fuego"},{"tipo":"volador"}],"debilidad":[{"tipo":"agua"},{"tipo":"electrico"},{"tipo":"roca"}]}/*sig
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/fuecoco.gif","power":600, "title":"Fuecoco","atack":[{"name":"Mordisco","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"},{"name":"Calcinación","power":55,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"},{"name":"Lanzallamas","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"},{"name":"Ascuas","power":55,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/fuego.gif"}],"tipo":[{"tipo":"fuego"}],"debilidad":[{"tipo":"agua"},{"tipo":"tierra"},{"tipo":"roca"}]}/*sig
+*/,{"img":"https://ypok.netlify.app/src/img/fondos/rowlet.gif","power":700, "title":"Rowlet","atack":[{"name":"Picotazo","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"},{"name":"Hoja\n afilada","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"},{"name":"Ataque\n Furía","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"},{"name":"Hoja\n Aguda","power":70,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"}],"tipo":[{"tipo":"planta"},{"tipo":"volador"}],"debilidad":[{"tipo":"fuego"},{"tipo":"volador"},{"tipo":"hielo"},{"tipo":"veneno"},{"tipo":"roca"}]}/*sig
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/bulbasaur.gif","power":500, "title":"Bulbazur","atack":[{"name":"Latigo\n Cepa","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"},{"name":"Polvo\n Veneno","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"},{"name":"Bomba\n Germen","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"},{"name":"somnifero","power":55,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/hojas.gif"}],"tipo":[{"tipo":"veneno"},{"tipo":"planta"}],"debilidad":[{"tipo":"fuego"},{"tipo":"volador"},{"tipo":"hielo"},{"tipo":"psiquico"}]}/*sig
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/squirtle.gif","title":"Squirtle","power":600,"atack":[{"name":"Burbuja","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"},{"name":"Agua\n Cola","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"},{"name":"hidro\n pulso","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"},{"name":"cabezazo","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"}],"tipo":[{"tipo":"agua"}],"debilidad":[{"tipo":"planta"},{"tipo":"electrico"}]}/*sig
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/pikachu.gif","power":800, "title":"pikachu","atack":[{"name":"Impac\n trueno","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"},{"name":"Látigo","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"},{"name":"Bola\n Voltio","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"},{"name":"Chispazo","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"}],"tipo":[{"tipo":"electrico"}],"debilidad":[{"tipo":"tierra"}],"evolucion":[{"name":"raychu","img":{"img":"https://ypok.netlify.app/src/img/fondos/raichu.gif"},"power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]}/*sig
 */ ,{
-    "name":"https://ypok.netlify.app/src/img/fondos/dodrio.gif","title":"Dodrio","power":"500","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"normal"},{"tipo":"volador"}]
+    "img":"https://ypok.netlify.app/src/img/fondos/dodrio.gif","title":"Dodrio","power":900,"atack":[{"name":"Ataque\n Furia","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Doble\n Golpe","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Pico\n Taladro","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Picoteo","power":55,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"}],"tipo":[{"tipo":"normal"},{"tipo":"volador"}],"debilidad":[{"tipo":"hielo"},{"tipo":"electrico"},{"tipo":"roca"}]
 }/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/onix.gif","power":"500", "title":"Onix","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"roca"},{"tipo":"tierra"}]}/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/magikarp.gif","power":500, "title":"Magikarp","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"agua"}],"evolucion":[{"name":"gyarados","power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]}/*sig
-*/ ,{"name":"https://ypok.netlify.app/src/img/fondos/snorlax.gif","power":500, "title":"Snorlax","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"normal"}],"evolucion":[{"name":"Snorlax Gigamax","power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]}/*sig
-*/,{"name":"https://ypok.netlify.app/src/img/fondos/zapdos.gif","power":500, "title":"Zapdos","atack":[{"name":"Latigo\n Cepa","power":50},{"name":"Polvo\n Veneno","power":60},{"name":"Bomba\n Germen","power":65},{"name":"Placaje","power":50}],"tipo":[{"tipo":"electrico"},{"tipo":"volador"},{"tipo":"lucha"}],"evolucion":[{"name":"gyarados","power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]},];
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/onix.gif","power":799, "title":"Onix","atack":[{"name":"Lanza\n Rocas","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"avalancha","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Tumba\n Rocas","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Furia","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"}],"tipo":[{"tipo":"roca"},{"tipo":"tierra"}],"debilidad":[{"tipo":"acero"},{"tipo":"lucha"},{"tipo":"hielo"},{"tipo":"planta"},{"tipo":"tierra"}]}/*sig
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/magikarp.gif","power":450, "title":"Magikarp","atack":[{"name":"Salpicadura","power":40,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"},{"name":"Azote","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"},{"name":"Salto \n Cola","power":35,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"},{"name":"Placaje","power":50,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/agua.gif"}],"tipo":[{"tipo":"agua"}],"debilidad":[{"tipo":"planta"},{"tipo":"electrico"}],"evolucion":[{"name":"gyarados","power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]}/*sig
+*/ ,{"img":"https://ypok.netlify.app/src/img/fondos/snorlax.gif","power":1000, "title":"Snorlax","atack":[{"name":"Golpe \n Cuerpo","power":70,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Ronquido","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Giga\n Impacto","power":70,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"},{"name":"Tambor","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/normal.gif"}],"tipo":[{"tipo":"normal"}],"debilidad":[{"tipo":"lucha"}],"evolucion":[{"name":"Snorlax Gigamax","power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]}/*sig
+*/,{"img":"https://ypok.netlify.app/src/img/fondos/zapdos.gif","power":800, "title":"Zapdos","atack":[{"name":"Impac\n Trueno","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"},{"name":"Agilidad","power":60,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"},{"name":"Pico\n Taladro","power":65,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"},{"name":"Trueno","power":70,"gif":"https://ypok.netlify.app/src/img/fondos/efectos/rayo.gif"}],"tipo":[{"tipo":"electrico"},{"tipo":"volador"}],"debilidad":[{"tipo":"hielo"},{"tipo":"roca"}],"evolucion":[{"name":"gyarados","power":1000,"atack":[],"tipo":"agua"},{"tipo":"volador"}],"evolucion":[{"name":"","title":"Megagyarados","power":1500,"atack":""}]},];
 
 //funcion aleatoria
 function random(){
@@ -25,12 +25,13 @@ function random(){
     var randomn=random();
     console.log(random);
     pokedec[randomn].atack.map(item=>
-        cpuatacks.push({"name":item.name,"power":item.power})
+        cpuatacks.push({"name":item.name,"power":item.power,"git":item.gif})
         );
+        console.log(cpuatacks);
 
     console.log(cpuatacks);
     return new Promise(resolve=>{
-       setTimeout(()=>{resolve(character_2.innerHTML=`<span class="person2 img" style="background-image: url(${pokedec[randomn].name});"><span class="beat-container two conttwo" ></span><</span>`,cpu.innerHTML=`<strong>CPU poder:</strong> ${pointcpu=pokedec[randomn].power}`);b1.disabled=false;
+       setTimeout(()=>{resolve(character_2.innerHTML=`<span class="person2 img" style="background-image: url(${pokedec[randomn].img});"><span class="beat-container two conttwo" ></span></span>`,cpu.innerHTML=`<strong>CPU poder:</strong> ${pointcpu=pokedec[randomn].power}`);b1.disabled=false;
        b2.disabled=false;
        b3.disabled=false;
        b4.disabled=false;},510); 
@@ -45,7 +46,7 @@ async function jugadortwo(){
 function jugar(id){
     console.log("funcion iniciada");
     console.log(pokedec[id].title);
-   character_1.innerHTML=`<span class="person1 img" style="background-image: url(${pokedec[id].name});"><span class="beat-container one contone" ></span></span>`;
+   character_1.innerHTML=`<span class="person1 img" style="background-image: url(${pokedec[id].img});"><span class="beat-container one contone" ></span></span>`;
    j1.innerHTML=`<strong>Tú poder:</strong> ${pointj1=pokedec[id].power}`;
    setTimeout(()=>{jugadortwo();},1000);
     change(id);
@@ -55,7 +56,7 @@ function jugar(id){
 function change(id){
     title_choose.innerHTML=`Juega ya!`;
     var j=1;
-    let view=`${pokedec[id].atack.map(item=>`<input type="button"  class="buttonsj1${j++}" value="${item.name}" onclick="atack(${item.power})" >`)}`;
+    let view=`${pokedec[id].atack.map(item=>`<input type="button"  class="buttonsj1${j++}" value="${item.name}" onclick="atack(${item.power},${item.gif})" >`)}`;
     chooseplayer.innerHTML=view;
     b1=document.querySelector(".buttonsj11");
     b2=document.querySelector(".buttonsj12");
@@ -67,13 +68,13 @@ function change(id){
     b4.disabled=true;
 }
 //funciones ataques
-function atack(power){
+function atack(power,gif){
     b1.disabled=true;
     b2.disabled=true;
     b3.disabled=true;
     b4.disabled=true;
     var beat_container=document.querySelector(".conttwo");
-    beat_container.innerHTML=`<span class="beat2 beat img" style="background-image: url(https://ypok.netlify.app/src/img/fondos/ouch1.png);width:100px;height:100px; position:absolute; bottom:-5px;right:0;"></span>`;
+    beat_container.innerHTML=`<span class="beat2 beat img" style="background-image: url(${gif});width:100px;height:150px; position:absolute; bottom:-5px;right:0;"></span>`;
     pointr(2,power);
     setTimeout(()=>{removeimg(2);console.log("funciona el removeimg");},800);
     console.log("power j1"+pointj1);
@@ -84,9 +85,11 @@ function atack(power){
 }
 //funcion contraataque
 function contraataque(){
-    let powercpu=cpuatacks[random()].power;
+    let randomn=random();
+    let powercpu=cpuatacks[randomn].power;
+    let gif=cpuatacks[randomn].gif;
     let beat_container1=document.querySelector(".contone");
-    beat_container1.innerHTML=`<span class="beat1 beat img" style="background-image: url(https://ypok.netlify.app/src/img/fondos/ouch1.png);width:100px;height:100px;transform: scaleX(-1);position:absolute;bottom:125px; right:50px;"></span>`;
+    beat_container1.innerHTML=`<span class="beat1 beat img" style="background-image: url(${gif});width:100px;height:150px;transform: scaleX(-1);position:absolute;bottom:125px; right:50px;"></span>`;
     pointr(1,powercpu);
     console.log(pointj1);
     if(pointj1<=0 && pointj1<pointcpu){var conttwo=document.querySelector(".conttwo");
@@ -109,11 +112,11 @@ function contraataque(){
 function removeimg(op){
        //area de golpes2
        if(op==2){
-        let beat2=document.querySelector(".beat2");
-    beat2.classList.remove("img");
+        let beat2=document.querySelector(".conttwo");
+    beat2.innerHTML='';
        }else if(op==1){
-        let beat1=document.querySelector(".beat1");
-    beat1.classList.remove("img");
+        let beat1=document.querySelector(".contone");
+    beat1.innerHTML='';
        }
    
 }
